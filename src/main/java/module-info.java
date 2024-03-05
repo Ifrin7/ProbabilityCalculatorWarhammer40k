d59991d4be2fdc3e9e75433a4010ea8a1551b205
root <root@DESKTOP-3SLJHU7>
2024-03-05 22:22:55 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.probabilitycalculatorwarhammer40k {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires net.synedra.validatorfx;
+            requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            requires eu.hansolo.tilesfx;
+            requires com.almasb.fxgl.all;
+    
+    opens com.example.probabilitycalculatorwarhammer40k to javafx.fxml;
+    exports com.example.probabilitycalculatorwarhammer40k;
+}
